9d0c8b75e0af9a46a65ab5fb839c171f169ea599
Jakub Parada <5pjakub@gmail.com>
2025-08-21 02:16:06 +0200

    Feat: Lossless training

---
+++ coolchic/lossless/util/misc.py
@@ -0,0 +1,22 @@
+import os
+
+def clean_workdir(workdir_path: str) -> None:
+    """
+    Clean the working directory by removing all files and subdirectories.
+
+    Args:
+        workdir_path (str): The path to the working directory to clean.
+    Returns:
+        None
+    """
+    if os.path.exists(workdir_path):
+        print(f"Removing {workdir_path}...")
+        for file in os.listdir(workdir_path):
+            file_path = os.path.join(workdir_path, file)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    os.rmdir(file_path)
+            except Exception as e:
+                print(f"Failed to delete {file_path}. Reason: {e}")
